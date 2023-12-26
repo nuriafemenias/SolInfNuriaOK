bfe2ebee2766b6ec4b932952a7216d677f3a899f
nuriafemenias <nuriaorozcoloca333@gmail.com>
2023-12-26 18:42:11 +0100

    .

--- src/balletApp/balletOK/GUI.java
+++ src/balletApp/balletOK/GUI.java
@@ -19,7 +19,7 @@ public class GUI {
     Fonts fonts;
 
     // Declaració dels components de la GUI
-    Button b1;
+    Button b1,b2, b3, b4;
     // Music Player
     MusicPlayer mp;
 
@@ -36,38 +36,56 @@ public class GUI {
         // Creació dels Botons
             //dibuijaPantallaRegistro
         b1 = new Button(p5, "Entra", 440+550+20, 220+5*40+5*20+80, 200, 50);
-        b1.setColors(colores.getColorAt(3), 0, colores.getColorAt(2), colores.getColorAt(6));
+        b1.setColors(colores.getColorAt(0), 0, colores.getColorAt(2), colores.getColorAt(3));
         //b1.setFont(fonts.getSecondFont());
+        b2 = new Button(p5, "¿Está ya registrado?", 440+10, 220+5*40+5*20+80, 540, 50);
+        b2.setColors(colores.getColorAt(0), 0, colores.getColorAt(2), colores.getColorAt(3));
+        //b2.setFont(fonts.getSecondFont());
+            //dibujaMenu
+        b3 = new Button(p5, "Tu cuenta", 50, margeV+usuariRadi+10+75, 250, 70);
+        b3.setColors(colores.getColorAt(0), 0, colores.getColorAt(2), colores.getColorAt(3));
+        //b3.setFont(fonts.getSecondFont());
+        b4 = new Button(p5, "Canciones", 50, margeV+usuariRadi+10+80+30+70, 250, 70);
+        b4.setColors(colores.getColorAt(0), 0, colores.getColorAt(2), colores.getColorAt(3));
+        //b1.setFont(fonts.getSecondFont());
+            //botonsTuCuenta
+
+
 
         // Creació del Music Player
         mp = new MusicPlayer(p5,120, p5.height/3);
         mp.setSound(p5, "saxo.wav");
 
+
+
         // Creació del Text Field
             // dibuijaPantallaRegistro
         tf1 = new TextField(p5, 440, 220, 790, 40);
-        tf1.setColors(colores.getColorAt(3), 0, 0, colores.getColorAt(2));
+        tf1.setColors(colores.getColorAt(5), 0, 0, colores.getColorAt(2));
         tf1.setFont(fonts.getThirdFont());
         tf2 = new TextField(p5, 440, 220+40+20, 790, 40);
-        tf2.setColors(colores.getColorAt(3), 0, 0, colores.getColorAt(2));
+        tf2.setColors(colores.getColorAt(5), 0, 0, colores.getColorAt(2));
         tf2.setFont(fonts.getThirdFont());
         tf3 = new TextField(p5, 440, 220+2*40+2*20, 790, 40);
-        tf3.setColors(colores.getColorAt(3), 0, 0, colores.getColorAt(2));
+        tf3.setColors(colores.getColorAt(5), 0, 0, colores.getColorAt(2));
         tf3.setFont(fonts.getThirdFont());
         tf4 = new TextField(p5, 440, 220+3*40+3*20, 550, 40);
-        tf4.setColors(colores.getColorAt(3), 0, 0, colores.getColorAt(2));
+        tf4.setColors(colores.getColorAt(5), 0, 0, colores.getColorAt(2));
         tf4.setFont(fonts.getThirdFont());
         tf5 = new TextField(p5, 440, 220+4*40+4*20, 550, 40);
-        tf5.setColors(colores.getColorAt(3), 0, 0, colores.getColorAt(2));
+        tf5.setColors(colores.getColorAt(5), 0, 0, colores.getColorAt(2));
         tf5.setFont(fonts.getThirdFont());
     }
 
     public void dibujaPantallaRegistro(PApplet p5){
+        // Pantalla
         p5.background(255);
         dibujaPhoto1(p5);
         dibujaUsuari(p5);
         dibujaRect1(p5);
+        // Botones y otros
         b1.display(p5);
+        b2.display(p5);
         tf1.display(p5);
         tf2.display(p5);
         tf3.display(p5);
@@ -76,90 +94,117 @@ public class GUI {
     }
 
     public void dibujaFullMenu(PApplet p5){
+        // Pantalla
         p5.pushStyle();
         dibujaMenu(p5);
         p5.popStyle();
         dibujaPhoto2(p5);
         dibujaUsuariMenu(p5);
+        // Botones y otros
+        b3.display(p5);
+        b4.display(p5);
     }
 
     public void dibujaPantallaFavoritos(PApplet p5){
+        // Pantalla
         p5.background(255);
         dibujaFullMenu(p5);
         dibujaRect2(p5);
         dibujaBotonsTuCuenta(p5);
+        // Botones y otros
     }
 
     public void dibujaPantallaTusCanciones(PApplet p5){
+        // Pantalla
         p5.background(255);
         dibujaFullMenu(p5);
         dibujaRect2(p5);
         dibujaBotonsTuCuenta(p5);
+        // Botones y otros
     }
 
     public void dibujaPantallaAgregarCancion(PApplet p5){
+        // Pantalla
         p5.background(255);
         dibujaFullMenu(p5);
         dibujaRect2(p5);
         dibujaBotonsTuCuenta(p5);
+        // Botones y otros
     }
 
     public void dibujaPantallaTusListas(PApplet p5){
+        // Pantalla
         p5.background(255);
         dibujaFullMenu(p5);
         dibujaRect2(p5);
         dibujaBotonsTuCuenta(p5);
+        // Botones y otros
     }
 
     public void dibujaPantallaCancionesTusListas(PApplet p5){
+        // Pantalla
         p5.background(255);
         dibujaFullMenu(p5);
         dibujaRect2(p5);
         dibujaBotonsTuCuenta(p5);
+        // Botones y otros
     }
 
     public void dibujaPantallaAgregarLista(PApplet p5){
+        // Pantalla
         p5.background(255);
         dibujaFullMenu(p5);
         dibujaRect2(p5);
         dibujaBotonsTuCuenta(p5);
+        // Botones y otros
     }
 
     public void dibujaPantallaCanciones(PApplet p5){
+        // Pantalla
         p5.background(255);
         dibujaFullMenu(p5);
         dibujaRect34(p5);
+        // Botones y otros
     }
 
     public void dibujaPantallaCalentamiento(PApplet p5){
+        // Pantalla
         p5.background(255);
         dibujaFullMenu(p5);
         dibujaRect2(p5);
         dibujaBotonsCanciones(p5);
-        // Dibuixa el MusicPlayer
         mp.display(p5);
+        // Botones y otros
     }
 
     public void dibujaPantallaCoreografia(PApplet p5){
+        // Pantalla
         p5.background(255);
         dibujaFullMenu(p5);
         dibujaRect2(p5);
         dibujaBotonsCanciones(p5);
+        // Botones y otros
     }
 
     public void dibujaPantallaSubListas(PApplet p5){
+        // Pantalla
         p5.background(255);
         dibujaFullMenu(p5);
         dibujaRect2(p5);
         dibujaBotonsCanciones(p5);
+        // Botones y otros
     }
 
     public void dibujaPantallaSubListasCanciones(PApplet p5){
+        // Pantalla
         p5.background(255);
         dibujaFullMenu(p5);
         dibujaRect2(p5);
         dibujaBotonsCanciones(p5);
+        // Botones y otros
     }
+
+
 
 
     //  DIBUIX ELEMENTS, zones GUI
